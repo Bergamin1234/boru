89d86313b340e7d4107546e4b34c205df8eb8820
Bergamin <italobergamin321@gmail.com>
2026-09-16 13:09:22 -0400

    First Commit Project Boru

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  // O React está rodando no fundo. 
+  // No momento ele não renderiza nada para não apagar o seu site original!
+  // Vamos usar isso depois para criar a tela de "Área do Aluno / Admin" por cima.
+  return null;
+}
+
+export default App;
